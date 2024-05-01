f91b7364e07465740f79ccfe4467178293f83a88
Ganesh Kusumbe <gkusumbe7@gmail.com>
2024-05-01 17:05:17 +0530

    added code

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,4 +1,5 @@
 import foodImg from "../../assets/food-logo.png"
+
 function Header(){
     return (
         <>
@@ -15,7 +16,7 @@ function Header(){
                         </svg>
                     </button>
                 </div>
-                <div class="hidden md:flex md:items-center gap-2">
+                <div class="hidden md:flex md:items-center gap-2 ">
                     <a href="#" class="text-white font-semibold hover:text-yellow-200 px-3 py-3 hover:shadow-2xl hover:bg-gray-800">HOME</a>
                     <a href="#" class="text-white font-semibold hover:text-yellow-300 px-3 py-3 hover:shadow-2xl hover:bg-gray-800">SERVICES</a>
                     <a href="#" class="text-white font-semibold hover:text-yellow-300 px-3 py-3 hover:shadow-2xl hover:bg-gray-800">ABOUT US</a>
@@ -26,7 +27,7 @@ function Header(){
             </div>
         </nav>
         
-        <div id="mobileMenu" class="md:hidden hidden bg-gray-800">
+        <div id="mobileMenu" class="md:hidden hidden bg-gray-800 ">
             <a href="#" class="block text-white hover:text-gray-300 px-4 py-2 hover:font-bold hover:bg-gradient-to-r from-violet-600 to-black hover:border hover:border-yellow-500">HOME</a>
             <a href="#" class="block text-white hover:text-gray-300 px-4 py-2 hover:font-bold hover:bg-gradient-to-r from-violet-600 to-black hover:border hover:border-yellow-500">SERVICES</a>
             <a href="#" class="block text-white hover:text-gray-300 px-4 py-2 hover:font-bold hover:bg-gradient-to-r from-violet-600 to-black hover:border hover:border-yellow-500">ABOUT US</a>
